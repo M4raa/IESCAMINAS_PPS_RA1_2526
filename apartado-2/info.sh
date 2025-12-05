ae5e10913e0aa4a8ba153c28af9cfaf3b3245b7e
M4raa <davidmarpons@gmail.com>
2025-12-05 19:47:28 +0100

    Script BASH multisistema

--- apartado-2/info.sh
+++ apartado-2/info.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
-MAC_ADDRESS=$(ip link show | awk '/ether/ {print $2; exit}')
-OS_INFO=$(uname -o)
+# OS detection
+OS_TYPE=$(uname -s)
+
+# Common info by OS
 HOSTNAME=$(hostname)
 CURRENT_USER=$(whoami)
+ARCHITECTURE=$(uname -m)
 
-echo "MAC del equipo: $MAC_ADDRESS
-Sistema Operativo: $OS_INFO
-Nombre del equipo: $HOSTNAME
-Usuario: $CURRENT_USER"
+# Specific info by OS
+declare MAC
+declare IP
+
+case $OS_TYPE in
+    # Linux
+    Linux)
+        MAC=$(ip link show | grep link/ether | awk '{print $2}')
+        IP=$(hostname -I | awk '{print $1}')
+        ;;
+    # macOS
+    Darwin)
+        MAC=$(ifconfig en0 | grep ether | awk '{print $2}')
+        IP=$(ifconfig en0 | grep inet | awk '{print $2}')
+        ;;
+    # Windows (Git Bash)
+    CYGWIN*|MINGW*|MSYS*)
+        IP=$(powershell.exe -noprofile -command "Get-NetIPConfiguration | Where-Object { \$_.IPv4DefaultGateway -ne \$null } | Select-Object -ExpandProperty IPv4Address | Select-Object -ExpandProperty IPAddress")
+        MAC=$(powershell.exe -noprofile -command "Get-NetIPConfiguration | Where-Object { \$_.IPv4DefaultGateway -ne \$null } | Select-Object -ExpandProperty NetAdapter | Select-Object -ExpandProperty MacAddress")
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+# Show results
+echo "System info"
+echo "OS: $OS_TYPE"
+echo "Architecture: $ARCHITECTURE"
+echo "Hostname: $HOSTNAME"
+echo "User: $CURRENT_USER"
+echo "IP: $IP"
+echo "MAC: $MAC"
